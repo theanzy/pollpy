2873a066e2e53eafc8dda28a5594a476aa40a753
tzy <theanzy1@gmail.com>
2023-12-19 11:53:37 +0800

    fix: bas64 short uuid

--- src/routes/(app)/[slug]/+page.server.ts
+++ src/routes/(app)/[slug]/+page.server.ts
@@ -1,17 +1,14 @@
 import { db } from '$lib/server/drizzle.js';
 import { answers, polls } from '$lib/server/schema/poll.js';
 import { users } from '$lib/server/schema/user.js';
+import { base64toUUID } from '$lib/utils.js';
 import { eq } from 'drizzle-orm';
 
-// NDc5NTk4NDEtYjZlOC00NjhjLWFlOGItNTA3MTZjNzc4MGVj
+// HVFCzy6uRgqcBK7o9OtRpQ
 // NDRjNjc4M2QtMjI2OC00MTRkLWIyYTMtMTI1YmZmOGJmZGJj
 
 export async function load({ params }) {
 	const slug = params.slug;
-	const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
-	if (!base64regex.test(slug)) {
-		return { poll: null };
-	}
 
 	const res = await db
 		.select({
@@ -27,7 +24,7 @@ export async function load({ params }) {
 		.from(polls)
 		.leftJoin(users, eq(users.id, polls.createdBy))
 		.innerJoin(answers, eq(polls.id, answers.pollId))
-		.where(eq(polls.id, atob(slug)));
+		.where(eq(polls.id, base64toUUID(slug)));
 
 	type PollWithAnswers = Omit<(typeof res)[0], 'answer'> & {
 		answers: Array<(typeof res)[0]['answer']>;
