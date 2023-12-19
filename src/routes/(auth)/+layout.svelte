b52c644159dc96729c8e926f86b12c66f20cfa98
tzy <theanzy1@gmail.com>
2023-12-19 13:42:41 +0800

    home back link auth page

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,3 +1,21 @@
 <main class="my-auto min-h-[100dvh] flex justify-center items-center">
-	<slot />
+	<div class="max-w-5xl px-5 md:px-0 mx-auto w-full flex flex-col gap-2">
+		<a
+			href="/"
+			class="max-w-lg mx-auto flex flex-row items-center gap-2 w-full mb-5 transition hover:text-primary-100 outline-none focus-visible:text-primary-100"
+		>
+			<svg
+				class="w-5 h-5"
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"><path fill="currentColor" d="M4 21V9l8-6l8 6v12h-6v-7h-4v7z" /></svg
+			>
+
+			Home
+		</a>
+		<div class="w-full flex flex-row justify-center items-center">
+			<slot />
+		</div>
+	</div>
 </main>
--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -11,33 +11,31 @@
 	}
 </script>
 
-<div class="max-w-5xl px-5 md:px-0 flex flex-row justify-center items-center w-full">
-	<div class="w-full max-w-lg">
-		<h1 class="text-surface-50 text-3xl font-semibold text-center mb-3">Log In</h1>
-		<form method="post" class="flex flex-col gap-1 mx-auto" use:enhance>
-			<label for="username">Username</label>
-			<Input name="username" id="username" class="mb-2" required />
-			<label for="password">Password</label>
-			<Input type="password" name="password" id="password" class="mb-6" required />
-			<button
-				type="submit"
-				class="rounded-sm bg-primary-600 text-surface-50 py-1 px-3 focus-visible:ring-primary-500 focus-visible:ring-offset-2 focus-visible:ring-1 outline-none focus-visible:ring-offset-surface-900 transition enabled:hover:text-white enabled:hover:bg-primary-700 enabled:focus-visible:bg-primary-700"
+<div class="w-full max-w-lg">
+	<h1 class="text-surface-50 text-3xl font-semibold text-center mb-3">Log In</h1>
+	<form method="post" class="flex flex-col gap-1 mx-auto" use:enhance>
+		<label for="username">Username</label>
+		<Input name="username" id="username" class="mb-2" required />
+		<label for="password">Password</label>
+		<Input type="password" name="password" id="password" class="mb-6" required />
+		<button
+			type="submit"
+			class="rounded-sm bg-primary-600 text-surface-50 py-1 px-3 focus-visible:ring-primary-500 focus-visible:ring-offset-2 focus-visible:ring-1 outline-none focus-visible:ring-offset-surface-900 transition enabled:hover:text-white enabled:hover:bg-primary-700 enabled:focus-visible:bg-primary-700"
+		>
+			Log In
+		</button>
+	</form>
+	<hr class="border-transparent border-b border-b-surface-800 my-5" />
+	<GoogleButton />
+	<div class="mt-4 w-full text-right">
+		<p class="text-sm text-right">
+			Don't have an account?
+			<a
+				href="/signup"
+				class="ml-1 text-center text-primary-700 font-medium transition hover:text-primary-600"
 			>
-				Log In
-			</button>
-		</form>
-		<hr class="border-transparent border-b border-b-surface-800 my-5" />
-		<GoogleButton />
-		<div class="mt-4 w-full text-right">
-			<p class="text-sm text-right">
-				Don't have an account?
-				<a
-					href="/signup"
-					class="ml-1 text-center text-primary-700 font-medium transition hover:text-primary-600"
-				>
-					Sign up
-				</a>
-			</p>
-		</div>
+				Sign up
+			</a>
+		</p>
 	</div>
 </div>
