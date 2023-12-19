9619d69350658d61999f14c632571280c7488f2b
tzy <theanzy1@gmail.com>
2023-12-19 14:07:44 +0800

    fix: vote checkbox name

--- src/routes/(app)/[slug]/+page.svelte
+++ src/routes/(app)/[slug]/+page.svelte
@@ -25,7 +25,7 @@
 						<input
 							class="relative float-left h-5 w-5 appearance-none rounded-full border-2 border-solid border-neutral-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary-500 checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary-500 checked:after:bg-primary-500 checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:border-primary-500 checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:border-neutral-600 dark:checked:border-primary-500 dark:checked:after:border-primary-500 dark:checked:after:bg-primary-500 dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:border-primary-500 dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
 							id={answer.id}
-							name="answer"
+							name={poll.maxChoice > 1 ? `answer.${answer.id}` : 'answer'}
 							type={poll.maxChoice > 1 ? 'checkbox' : 'radio'}
 							value={answer.id}
 						/>
