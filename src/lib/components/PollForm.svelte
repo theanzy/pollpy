67c478b8a02777385d62891462413b955145dcde
tzy <theanzy1@gmail.com>
2024-01-05 13:25:27 +0800

    fix poll close date toggle

--- src/lib/components/PollForm.svelte
+++ src/lib/components/PollForm.svelte
@@ -317,7 +317,7 @@
 		>
 			<div class="flex flex-col gap-4 md:flex-1 pr-3 text-surface-100">
 				<div class="flex flex-col gap-4">
-					<ToggleBlock open={initialData?.closedAt !== null}>
+					<ToggleBlock open={!!initialData?.closedAt}>
 						<div
 							slot="trigger"
 							let:toggle={toggleClosedDate}
